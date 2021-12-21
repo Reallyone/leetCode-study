ef4a97b0ef072d39f30f317fd24535e7ac9a4e12
jumper <18327646670@163.com>
2021-12-21 21:59:20 +0800

    1

--- src/main/java/com/strom/study/demo/SimpleQuestions.java
+++ src/main/java/com/strom/study/demo/SimpleQuestions.java
@@ -7,35 +7,28 @@ public class SimpleQuestions {
     /**
      * 加一
      * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
-     *
+     * <p>
      * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
-     *
+     * <p>
      * 你可以假设除了整数 0 之外，这个整数不会以零开头。
-     *
+     * <p>
      * 链接：https://leetcode-cn.com/problems/plus-one
-     *
      */
     public static int[] plusOne(int[] digits) {
         int len = digits.length;
-        if (digits[len-1] !=9)
-        {
-            digits[len-1] +=1;
-        }else
-        {
-            for (int i = len  - 1; i >= 0; i--) {
-                if (digits[i] ==9 )
-                {
-                    if (i==0)
-                    {
-                        digits = new int[digits.length+1];
-                        digits[0] =1;
-                    }else
-                    {
-                        digits[i]=0;
+        if (digits[len - 1] != 9) {
+            digits[len - 1] += 1;
+        } else {
+            for (int i = len - 1; i >= 0; i--) {
+                if (digits[i] == 9) {
+                    if (i == 0) {
+                        digits = new int[digits.length + 1];
+                        digits[0] = 1;
+                    } else {
+                        digits[i] = 0;
                     }
-                }else
-                {
-                    digits[i] +=1;
+                } else {
+                    digits[i] += 1;
                     break;
                 }
             }
@@ -43,11 +36,190 @@ public class SimpleQuestions {
         return digits;
     }
 
+    /**
+     * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+     * <p>
+     * 输入为 非空 字符串且只包含数字 1 和 0。
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static String addBinary(String a, String b) {
+        if (a == null || a.length() == 0) return b;
+        if (b == null || b.length() == 0) return a;
+
+        StringBuilder stb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        int c = 0;  // 进位
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) c += a.charAt(i--) - '0';
+            if (j >= 0) c += b.charAt(j--) - '0';
+            stb.append(c % 2);
+            c >>= 1;
+        }
+
+        String res = stb.reverse().toString();
+        return c > 0 ? '1' + res : res;
+    }
+
+    /**
+     * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+     * <p>
+     * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+     *
+     * @param x
+     * @return
+     */
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * <p>
+     * 注意：给定 n 是一个正整数。
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        if (n > 45) {
+            return 0;
+        }
+        int first = 1;
+
+        int send = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int tem = first + send;
+            first = send;
+            send = tem;
+        }
+        return send;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m, j = 0;
+        while (i < m + n) {
+            nums1[i++] = nums2[j++];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p ==null && q==null)
+        {
+            return true;
+        }else if (p ==null || q==null)
+        {
+            return false;
+        }else if( p.val != q.val)
+        {
+            return false;
+        }else
+        {
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+    }
+
+    public static int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            //闰年是29
+            ++amount[1];
+        }
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+
 
 
     public static void main(String[] args) {
-        int[] digits = new int[]{1,2,3};
-        System.out.println(Arrays.toString(plusOne(digits)));
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
     }
 
 
