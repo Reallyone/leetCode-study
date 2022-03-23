817690bf9cb3f1eca78262be6a9d34729df46cbe
hjx <18327646670@163.com>
2022-03-23 10:58:28 +0800

    1

--- src/main/java/com/strom/study/demo/TreeNodeStudy.java
+++ src/main/java/com/strom/study/demo/TreeNodeStudy.java
@@ -49,8 +49,59 @@ public class TreeNodeStudy {
         return max;
     }
 
+    /**
+     * 杨辉三角
+     * 第n行有n个数字
+     * 每一行的开始和结尾数字都为1.
+     * 第n+1行的第i个数字等于第n行的i-1个数字加上第n行的i个数字。
+     *
+     * @param numRows
+     * @return
+     */
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        int[][] arr = new int[numRows][numRows];
+        for (int i = 0; i < numRows; i++) {
+            System.out.println(i);
+            List<Integer> subList = new ArrayList<>();
+            for (int j = 0; j <=i; j++) {
+                if(j==0 || j==i){
+                    arr[i][j] = 1;
+                }else{
+                    arr[i][j] = arr[i-1][j-1]+arr[i-1][j];
+                }
+                subList.add(arr[i][j]);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
+
+    /**
+     * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+     *
+     * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+     * @param rowIndex
+     * @return
+     */
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> numbers = new ArrayList<>(rowIndex+1);
+        long cur = 1;
+
+        for (int i=0;i<=rowIndex;i++){
+            numbers.add((int) cur);
+            System.out.println(cur +"=="+(rowIndex-i));
+            cur = cur * (rowIndex -i)/(i+1);
+            System.out.println("----"+cur);
+        }
+        return numbers;
+    }
+
+
+
+
 
     public static void main(String[] args) {
-
+        System.out.println(getRow(5));
     }
 }
